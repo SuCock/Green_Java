c04eb6cdd185b2027d29b7169253634653901b5f
SuCock <ksymin313@gmail.com>
2024-03-04 17:25:37 +0900

    240304 오후 씨샵

---
+++ cs_work/mdiProject/mdiProject/todo/TodoDBManager.cs
@@ -0,0 +1,77 @@
+﻿using mdiProject.database;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdiProject.todo
+{
+    public class TodoDBManager
+    {
+        public bool insert(Todo todo)
+        {
+            try
+            {
+                OracleConnection conn = DBINFO.openConnect();
+                string sql = "INSERT INTO TODO (IDX, USERS_IDX, TITLE, CONTENT, FINISHDATE) VALUES (todoIdx.nextval, :useridx, :title, :content, :finishdate)";
+                OracleCommand cmd = new OracleCommand(sql, conn);
+                cmd.Parameters.Add(":useridx", todo.user_idx);
+                cmd.Parameters.Add(":title", todo.title);
+                cmd.Parameters.Add(":content", todo.content);
+                cmd.Parameters.Add(":finishdate", todo.finishdate);
+                cmd.ExecuteNonQuery();
+
+                DBINFO.closeConnect();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public void update(Todo todo)
+        {
+        }
+
+        public void delete(Todo todo)
+        {
+        }
+
+        public DataTable select()
+        {
+            try
+            {
+                OracleConnection conn = DBINFO.openConnect();
+
+                string sql = "SELECT * FROM todo";
+
+                OracleDataAdapter adapter = new OracleDataAdapter();
+                DataSet ds = new DataSet();
+
+                OracleCommand oracleCommand = new OracleCommand(sql, conn);
+                adapter.SelectCommand = oracleCommand;
+
+                adapter.Fill(ds);
+
+                DBINFO.closeConnect();
+                return ds.Tables[0];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            
+        }
+    }
+
+    
+}
--- cs_work/mdiProject/mdiProject/todo/TodoForm.cs
+++ cs_work/mdiProject/mdiProject/todo/TodoForm.cs
@@ -1,19 +1,14 @@
-﻿using mdiProject.user;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace mdiProject.todo
 {
     public partial class TodoForm : Form
     {
-        private static TodoForm instance; //  싱글톤으로 만들어주기
+        private static TodoForm instance; 
+        private TodoDBManager dbManager = new TodoDBManager();
         public static TodoForm getInstance()
         {
             if (instance == null || instance.IsDisposed)
@@ -29,6 +24,40 @@ namespace mdiProject.todo
         public TodoForm()
         {
             InitializeComponent();
+            comboBox1.DataSource = new List<string>()
+            {
+                "1", "2", "3", "4", "5"
+            };
+            comboBox1.SelectedText = "1";
+
+            Label title_lb = new Label();
+            title_lb.Text = "title";
+            title_lb.AutoSize = true;
+
+            Panel resevPanel = new Panel();
+            resevPanel.BackColor = Color.White;
+            resevPanel.Size = new Size(200, 100);
+
+            resevPanel.Controls.Add(title_lb);
+            panel1.Controls.Add(resevPanel);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool result = dbManager.insert(new Todo()
+            {
+                user_idx = int.Parse(comboBox1.Text),
+                title = textBox1.Text,
+                content = textBox2.Text,
+                finishdate = dateTimePicker1.Value
+            });
+
+            if (result)
+            {
+                MessageBox.Show("입력 성공했습니다");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
         }
     }
 }
